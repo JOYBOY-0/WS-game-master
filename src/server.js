a3dbb887859efabbbad534ecf29d84445370402d
Your Name <you@example.com>
2022-01-28 15:47:47 -0300

    '

--- src/server.js
+++ src/server.js
@@ -5,11 +5,6 @@ const port = process.env.PORT || 8000
 
 const server = Server(
   {games: [WsGame],
-  origins: [
-    // Allow your game site to connect.
-    'https://www.world-seekers.com/',
-    'https://www.world-seekers.com/arena'
-  ],
-  });
+  origins: ['https://www.world-seekers.com'] });
 
 server.run(8000, () => console.log('server running ' + port ));
