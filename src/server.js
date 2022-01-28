0f07dd2d5c2fcbe314980783dc2f77463a782b24
Your Name <you@example.com>
2022-01-28 15:24:07 -0300

    add origin cors

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const { WsGame } = require('./Game');
 
 const port = process.env.PORT || 8000
 
-const server = Server({games: [WsGame]});
+const server = Server(
+  {games: [WsGame],
+  origins: [
+    // Allow your game site to connect.
+    'https://www.world-seekers.com/',
+    // Allow localhost to connect, except when NODE_ENV is 'production'.
+    Origins.LOCALHOST_IN_DEVELOPMENT
+  ],
+  });
 
 server.run(8000, () => console.log('server running ' + port ));
