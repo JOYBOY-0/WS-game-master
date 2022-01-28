742ceba266f7dcee96addcba5c9d1306066255e4
Your Name <you@example.com>
2022-01-28 14:59:43 -0300

    origins removed

--- src/server.js
+++ src/server.js
@@ -1,10 +1,8 @@
-const { Server, Origins } = require('boardgame.io/server');
+const { Server } = require('boardgame.io/server');
 const { WsGame } = require('./Game');
 
 const port = process.env.PORT || 8000
 
 const server = Server({games: [WsGame]});
-
-Server.ge
 
 server.run(8000, () => console.log('server running ' + port ));
