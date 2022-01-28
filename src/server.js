a5cab94fa0329046a56a662ff5e577a0a04159fe
Your Name <you@example.com>
2022-01-28 16:04:44 -0300

    port correction

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const { Server, Origins } = require('boardgame.io/server');
 const { WsGame } = require('./Game');
 
-const port = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 const server = Server(
   {games: [WsGame],
   origins: ['https://www.world-seekers.com',
   Origins.LOCALHOST_IN_DEVELOPMENT] });
 
-server.run(8000, () => console.log('server running ' + port ));
+server.run(PORT);
