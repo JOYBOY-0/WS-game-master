5b999e9b678ddca8f6d5dc90701d5da273ca6cd7
JotaRosen <jrosenfeld@fi.uba.ar>
2022-01-28 14:55:32 -0300

    adding cors free pass

--- src/server.js
+++ src/server.js
@@ -3,14 +3,8 @@ const { WsGame } = require('./Game');
 
 const port = process.env.PORT || 8000
 
-const server = Server({
-  games: [WsGame],
-  origins: [
-    // Allow your game site to connect.
-    'https://www.world-seekers.com',
-    // Allow localhost to connect, except when NODE_ENV is 'production'.
-    Origins.LOCALHOST_IN_DEVELOPMENT
-  ],
-});
+const server = Server({games: [WsGame]});
+
+Server.ge
 
 server.run(8000, () => console.log('server running ' + port ));
